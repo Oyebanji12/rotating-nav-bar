b0ac0c0f08df96be4c0e29fb0ec9816a2e536c54
oyebanji12 <oyebanjiqudus81@gmail.com>
2022-06-06 08:04:02 +0100

    initial commit

---
+++ work.js
@@ -0,0 +1,52 @@
+/*
+var open= document.getElementById('open');
+var close= document.getElementById('close');
+var containers=document.querySelector('.container')
+
+open.addEventListener('click', function () {
+    containers.classList.add('show-nav')
+    
+})
+
+close.addEventListener('click', function () {
+    containers.classList.remove('show-nav')
+})
+*/
+
+
+var open=document.getElementById('open');
+var close=document.getElementById('close');
+var containers= document.querySelector('.container')
+
+
+open.addEventListener('click', function () {
+    containers.classList.add('show-nav')
+    
+})
+
+close.addEventListener('click', function () {
+    containers.classList.remove('show-nav')
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
